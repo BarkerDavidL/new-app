98908b6b0465919733a4a6b9c0d2214d600716c7
David Barker <dave@ncomtech.com>
2023-03-08 12:46:56 -0500

    split apart main content in index.html and put it in 5 html files in dist (setup, new, webpack, babel, git), added a navigation list then added javascript to add onclick functions to the navigation list items

--- src/index.js
+++ src/index.js
@@ -1,2 +1,36 @@
 console.log("In index.js");
 console.log("Now using webpack");
+
+function pageSet(newPage) {
+    let file = newPage;
+
+    fetch (file)
+        .then(response => response.text())
+        .then(responseText => document.getElementById("current-page").innerHTML = responseText);
+}
+
+function setPageOnClick() {
+    let list = document.getElementById('pagelist');
+    console.log("list is: "+list+":");
+    let listItems = list.getElementsByTagName('li');
+    console.log("listItems are: "+listItems+":");
+    [...listItems].forEach(element => {
+        console.log("adding onclick for "+element.value+", innerHTML: "+element.innerHTML);
+        if ("Setup" == element.innerHTML) {
+            element.setAttribute('onclick', "pageSet('setup.html')");
+        } else if ("New" == element.innerHTML) {
+            element.setAttribute('onclick', "pageSet('new.html')");
+        } else if ("Webpack" == element.innerHTML) {
+            element.setAttribute('onclick', "pageSet('webpack.html')");
+        } else if ("Babel" == element.innerHTML) {
+            element.setAttribute('onclick', "pageSet('babel.html')");
+        } else if ("Git" == element.innerHTML) {
+            element.setAttribute('onclick', "pageSet('git.html')");
+        }
+    });
+}
+
+window.onload = () => {
+    console.log("Window has loaded");
+    setPageOnClick();
+}
